7dd1cb986b5e5a97efa907799493619634983fed
T. Masuch <69604121+HackerFrosch@users.noreply.github.com>
2022-10-13 12:00:16 +0200

    Simplified the DarkMode-Function

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -1,29 +1,8 @@
-function checkMode() {
-    darkMode = document.querySelector('.dark-mode-switch input').checked;
-    newColor = 'black';
-    oldColor = 'white';
-    if (darkMode) {
-        newColor = 'white';
-        oldColor = 'black';
-    }
-    ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'label', 'span'].forEach(
-        (selector) => {
-            document.querySelectorAll(selector + ":not(.fixed-color)").forEach((element) => {
-                element.style.color = newColor;
-            });
-        }
-    );
-    ['.sidebar>div>p'].forEach((selector) => {
-        document.querySelectorAll(selector).forEach((element) => {
-            element.style.borderColor = newColor;
-        });
-    });
-    ['body', '.sidebar>div'].forEach((selector) => {
-        document.querySelectorAll(selector).forEach((element) => {
-            element.style.backgroundColor = oldColor;
-        });
-    });
+function switchDarkMode() {
+	darkMode = document.querySelector('.dark-mode-switch input').checked;
+	if (darkMode) {
+		document.body.classList.add("dark");
+	} else {
+		document.body.classList.remove("dark");
+	}
 }
-document.querySelector('.dark-mode-switch input').onclick = () => {
-    checkMode();
-};
