a1f9cffbddb3fba75a5e8824d37b4a4f8d84b7be
Theo Stefan <tgstefan@csd.auth.gr>
2023-01-26 16:42:19 +0200

    -added the "pointer-events: none" command, now the user can click on the green projection without any problem. -Deleted some unnecessary code.

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -1,5 +1,3 @@
-
-
 function switchDarkMode() {
 	darkMode = document.querySelector('.dark-mode-switch input').checked;
 	if (darkMode) {
@@ -20,49 +18,7 @@ function projectMovement() {
 	if (OnOff % 2 == 1)
 	{
 		ProjectMovement = true;
-
-		if (playerIndex % 7 > 0 && Math.floor(playerIndex / 7) > 1) {
-			drawToIndex(playerMovementProjection1, playerIndex - 15);
-			playerMovementProjection1.style.opacity = '60';
-		}
-		if (playerIndex % 7 < 6 && Math.floor(playerIndex / 7) > 1) {
-			drawToIndex(playerMovementProjection2, playerIndex - 13);
-			playerMovementProjection2.style.opacity = '60';
-		}
-		if (playerIndex % 7 > 1 && Math.floor(playerIndex / 7) > 0) {
-			drawToIndex(playerMovementProjection3, playerIndex - 9);
-			playerMovementProjection3.style.opacity = '60';
-		}
-		if (playerIndex % 7 < 5 && Math.floor(playerIndex / 7) > 0)
-		{
-			drawToIndex(playerMovementProjection4, playerIndex - 5);
-			playerMovementProjection4.style.opacity = '60';
-		}
-		if (playerIndex % 7 > 1 && Math.floor(playerIndex / 7) < 6) {
-			drawToIndex(playerMovementProjection5, playerIndex + 5);
-			playerMovementProjection5.style.opacity = '60';
-		}
-		if (playerIndex % 7 < 5 && Math.floor(playerIndex / 7) < 6) {
-			drawToIndex(playerMovementProjection6, playerIndex + 9);
-			playerMovementProjection6.style.opacity = '60';
-		}
-		if (playerIndex % 7 > 0 && Math.floor(playerIndex / 7) < 5) {
-			drawToIndex(playerMovementProjection7, playerIndex + 13);
-			playerMovementProjection7.style.opacity = '60';
-		}
-		if (playerIndex % 7 < 6 && Math.floor(playerIndex / 7) < 5) {
-			drawToIndex(playerMovementProjection8, playerIndex + 15);
-			playerMovementProjection8.style.opacity = '60';
-		}
-
-		playerMovementProjection1.style.zIndex = '0';
-		playerMovementProjection2.style.zIndex = '0';
-		playerMovementProjection3.style.zIndex = '0';
-		playerMovementProjection4.style.zIndex = '0';
-		playerMovementProjection5.style.zIndex = '0';
-		playerMovementProjection6.style.zIndex = '0';
-		playerMovementProjection7.style.zIndex = '0';
-		playerMovementProjection8.style.zIndex = '0';
+		drawPlayerPossibleMoves();
 	}
 	else
 	{
@@ -75,14 +31,5 @@ function projectMovement() {
 		playerMovementProjection6.style.opacity = '0';
 		playerMovementProjection7.style.opacity = '0';
 		playerMovementProjection8.style.opacity = '0';
-
-		playerMovementProjection1.style.zIndex = '-2';
-		playerMovementProjection2.style.zIndex = '-2';
-		playerMovementProjection3.style.zIndex = '-2';
-		playerMovementProjection4.style.zIndex = '-2';
-		playerMovementProjection5.style.zIndex = '-2';
-		playerMovementProjection6.style.zIndex = '-2';
-		playerMovementProjection7.style.zIndex = '-2';
-		playerMovementProjection8.style.zIndex = '-2';
 	}
 }
