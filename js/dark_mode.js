e9849c02f51b7fe28875143f18b76481f95fcf31
Pushpender Saini <54404738+PushpenderSaini0@users.noreply.github.com>
2024-10-19 02:06:28 +0530

    Persist dark mode

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -1,13 +1,3 @@
-function switchDarkMode() {
-	darkMode = document.querySelector('.dark-mode-switch input').checked;
-	if (darkMode) {
-		document.body.classList.add("dark");
-	} else {
-		document.body.classList.remove("dark");
-	}
-}
-
-
 function projectMovement() {
 	!PH.getStatus() ? PH.enable() : PH.disable();
 }
